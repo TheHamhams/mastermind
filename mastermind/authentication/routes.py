1c8a8e4f7a03f17a14c40bf4493061ec6808378f
TheHamhams <hamhams86@gmail.com>
2022-12-27 14:19:32 -0800

    added profile page and ability to update user info

--- mastermind/authentication/routes.py
+++ mastermind/authentication/routes.py
@@ -1,16 +1,16 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from mastermind.models import User, db, check_password_hash, users_schema, Scores
 
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
-from mastermind.forms import UserLoginForm, UserSignupForm
+from mastermind.forms import UserLoginForm, UserSignupForm, UserEmailUpdate, UserUsernameUpdate, UserPasswordUpdate
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
 @auth.route('/admin')
 def admin():
     users = User.query.all()
-    
+
     result = users_schema.dump(users)
     return jsonify(result)
 
@@ -19,74 +19,121 @@ def delete_score(id):
     score = Scores.query.get(id)
     db.session.delete(score)
     db.session.commit()
-    
+
     return jsonify({})
 
 @auth.route('/admin/<string:id>', methods=['DELETE'])
 def delete(id):
     user = User.query.filter_by(username = id).delete()
-    
+
     db.session.commit()
     return jsonify({})
 
+# Profile page
+@auth.route('/profile', methods=['GET', 'POST'])
+def profile():
+    email_form = UserEmailUpdate()
+    username_form = UserUsernameUpdate()
+    password_form = UserPasswordUpdate()
+    
+    user = User.query.filter_by(email=current_user.email).first()
+
+    score = user.high_score
+    username = user.username
+    email = user.email
+
+    if request.method == 'POST' and email_form.validate_on_submit():
+        new_email = email_form.email.data
+        check_email = User.query.filter(User.email == new_email).first()
+        if check_email:
+            flash('Email already exists', category='error')
+        else:
+            user.email = new_email
+            db.session.commit()
+        return redirect(url_for('auth.profile'))
+    if request.method == 'POST' and username_form.validate_on_submit():
+        new_username = username_form.username.data
+        check_username = User.query.filter(User.username == new_username).first()
+        if check_username:
+            flash('Username already exists', category='error')
+        else:
+            user.username = new_username
+            db.session.commit()
+            return redirect(url_for('auth.profile'))
+    if request.method == 'POST' and password_form.validate_on_submit():
+        new_password = password_form.password.data
+        if new_password != password_form.password_confirm.data:
+            flash('Passwords do not match', category='error')
+            return redirect(url_for('auth.profile'))
+        else:
+            user.password = generate_password_hash(new_password)
+            db.session.commit()
+            return redirect(url_for('auth.profile'))
+    
+
+    return render_template('profile.html', title='Profile', score=score, username=username, email=email, email_form=email_form, username_form=username_form, password_form=password_form)
+
+# Login page
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    
+
     form = UserLoginForm()
-    
+
     try:
         if request.method == 'POST' and form.validate_on_submit():
             email = form.email.data
             password = form.password.data
-            
+
             user = User.query.filter(User.email == email).first()
-           
+
             if user and check_password_hash(user.password, password):
                 login_user(user)
-            
-                flash(f'You have successfully logged in {email}', 'auth-success')
+
+                flash(f'You have successfully logged in {email}', category='error')
                 return redirect(url_for('site.home'))
             else:
-                flash('User info not found', 'auth-failed')
+                flash('User info not found', category='error')
                 return redirect(url_for('auth.login'))
     except:
         raise Exception('Invalid form data: Please check your form')
     return render_template('login.html', title="Login", form=form, user=current_user)
 
+# Signup page
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = UserSignupForm()
-    
+
     try:
         if request.method == 'POST' and form.validate_on_submit():
             email = form.email.data
             username = form.username.data
             password = form.password.data
             password_confirm = form.password_confirm.data
-            
+
             if password != password_confirm:
-                flash('Passwords do not match')
+                flash('Passwords do not match', category='error')
                 return redirect(url_for('auth.signup'))
-            
+
             user = User.query.filter_by(email=email).first()
-            
+
             if user:
-                flash('Email already exists', 'auth-failed')
+                flash('Email already exists', category='error')
                 return redirect(url_for('auth.signup'))
 
             user = User(email=email, username=username, password=password)
-            
+
             db.session.add(user)
             db.session.commit()
-            
+
             flash(f"You have successfully created a user account for { email }", 'success')
             return redirect(url_for('auth.login'))
-        
+
     except:
         raise Exception('Invalid form data: Please check your form')
-    
+
     return render_template('signup.html', title="Sign Up", form=form, user=current_user)
 
+# Logout
 @auth.route('/logout')
 @login_required
 def logout():
--- mastermind/forms.py
+++ mastermind/forms.py
@@ -5,11 +5,24 @@ from wtforms.validators import DataRequired, Email, Length
 class UserLoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit_button = SubmitField('Login')
+    submit_button = SubmitField('Submit')
     
 class UserSignupForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired(), Length(max=15)])
     password = PasswordField('Password', validators=[DataRequired()])
     password_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
-    submit_button = SubmitField('Sign Up')
+    submit_button = SubmitField('Submit')
+    
+class UserEmailUpdate(FlaskForm):
+    email = StringField('Email', validators=[Email(), DataRequired()])
+    submit_button = SubmitField('Update Email')
+    
+class UserUsernameUpdate(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(max=15)])
+    submit_button = SubmitField('Update Username')
+    
+class UserPasswordUpdate(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit_button = SubmitField('Update Password')
