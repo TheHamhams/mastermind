6914d1904ae98c9fae0a5b99205040f4019a670b
TheHamhams <hamhams86@gmail.com>
2023-01-02 09:12:44 -0800

    removed marshmallow

--- mastermind/__init__.py
+++ mastermind/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .config import Config
 from flask_migrate import Migrate
-from .models import db as root_db, login_manager, ma
+from .models import db as root_db, login_manager
 from flask_cors import CORS
 from .site.routes import site
 from .authentication.routes import auth
@@ -19,5 +19,4 @@ app.register_blueprint(api)
 app.config.from_object(Config)
 login_manager.init_app(app)
 root_db.init_app(app)
-ma.init_app(app)
 migrate = Migrate(app, root_db)
